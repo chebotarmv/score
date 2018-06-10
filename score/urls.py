f873546e4e850ae7d5edfd79d30a4195eca05124
chebotarmv <chebotarmv@gmail.com>
2018-06-10 17:34:18 +0300

    modified teams

---
+++ score/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^home/', views.home_page, name='home_page'),
+    url(r'^khl', views.khl, name='khl'),
+    url(r'^nhl', views.nhl, name='nhl'),
+]
+"""
+
+
+url(r'home/khl/team', views.khl_team, name='khl_team'),
+url(r'home/nhl/team', views.nhl_team, name='nhl_team'),
+url(r'home/khl/gamestat', views.khl_gamestat, name='khl_gamestat'),
+url(r'home/nhl/gamestat', views.nhl_gamestat, name='nhl_gamestat'),
+url(r'home/khl/archive', views.khl_archive, name='khl_archive'),
+url(r'home/nhl/archive', views.nhl_archive, name='nhl_archive'),
+url(r'home/makedata', views.makedata, name='make_data'),
+url(r'datacheck', views.datacheck, name='data_check')"""
--- score/views.py
+++ score/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import KhlTeam, NhlTeam
 
-# Create your views here.
+def home_page(request):
+    return render(request, 'score/home_page.html', {})
+
+def khl(request):
+    khl_teams = KhlTeam.objects.all()
+    return render(request, 'score/khl_page.html', {'khl_teams': khl_teams})
+
+def nhl(request):
+    nhl_teams = NhlTeam.objects.all()
+    return render(request, 'score/nhl_page.html', {'nhl_teams': nhl_teams})
